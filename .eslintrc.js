4e35a77e7fdb49a153fd7142714da6c726d0cf23
David Tolman <david.tolman@victoriaplum.com>
2018-06-18 09:33:16 +0100

    Add ESLint as dev dependency
    Modified format of package.json in addition to above.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'extends': [
+        'airbnb-base',
+    ],
+    'env': {
+        'node': true
+    },
+    'rules': {
+        'indent': ['error', 4],
+        'curly': ['error', 'all'],
+        'no-param-reassign': ['error', { 'props': false }],
+        'padding-line-between-statements': [
+            'error',
+            { 'blankLine': 'always', 'prev': 'if', 'next': '*' },
+        ],
+        'no-new': 0,
+    },
+};
