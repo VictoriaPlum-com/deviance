fb5ad1351d0d681ca9c4354380c4d44f76cdc1de
David Tolman <david.tolman@victoriaplum.com>
2018-06-25 10:45:56 +0100

    Extend test group depth

---
+++ example/tests/groupB/subB/basicSubTest.js
@@ -0,0 +1,9 @@
+module.exports = {
+    '@tags': ['isolate'],
+    'Squirrel test': function squirrelTest(browser) {
+        browser
+            .url(`${browser.launchUrl}/squirrel.html`)
+            .waitForElementPresent('body', 1000)
+            .expect.element('h1').text.to.equal('Squirrel');
+    },
+};
