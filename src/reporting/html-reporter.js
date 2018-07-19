6a2066ca129f9bf548c3d9439d8adddc775426af
cyrulik <mdziegielowski@gmail.com>
2018-07-19 13:51:36 +0100

    Prompt for opening HTML report

--- src/reporting/html-reporter.js
+++ src/reporting/html-reporter.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
+const prompt = require('prompt');
+const opn = require('opn');
 
 module.exports = {
     write: (results) => {
@@ -21,6 +23,28 @@ module.exports = {
                 if (error) {
                     throw error;
                 }
+
+                prompt.start({ noHandleSIGINT: true });
+
+                const schema = {
+                    properties: {
+                        report: {
+                            description: 'Do you want to open generated report? (yes/no)',
+                            type: 'string',
+                            required: true,
+                        },
+                    },
+                };
+
+                prompt.get(schema, (errors, result) => {
+                    if (errors) {
+                        throw errors;
+                    }
+
+                    if (result.report === 'yes') {
+                        opn(reportPath, { wait: false });
+                    }
+                });
             });
         });
     },
